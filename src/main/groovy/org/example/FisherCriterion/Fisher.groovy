a2c7427b5461bf4bde201e9f3fb3acc9da941353
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2023-12-14 02:51:50 +0300

    Добавлена генерация по нормальному распределению в Фишера, исправления в Стьюденте

--- src/main/groovy/org/example/FisherCriterion/Fisher.groovy
+++ src/main/groovy/org/example/FisherCriterion/Fisher.groovy
@@ -6,33 +6,38 @@ import org.example.MyNormalDistribution
 
 class Fisher {
     static void main(String[] args) {
+        int n1 = 30
+        double mean1 = 20
+        double standardDeviation1 = 3
+        int n2 = 30
+        double mean2 = 22
+        double standardDeviation2 = 3
+
         // Имеем две группы данных для проверки равенства дисперсий
-        def group1 = MyNormalDistribution.generateNormalDistribution(15, 20 as double, 3 as double)
-        def group2 = MyNormalDistribution.generateNormalDistribution(15, 20 as double, 3 as double)
+        def group1 = MyNormalDistribution.generateNormalDistribution(n1, mean1 as double, standardDeviation1 as double)
+        def group2 = MyNormalDistribution.generateNormalDistribution(n2, mean2 as double, standardDeviation2 as double)
 
         // Задаем уровень значимости (alpha)
         def alpha = 0.05
 
-        // 1. Вычисление выборочных дисперсий для каждой группы и вычисление поправленных дисперсий
+        // Вычисление выборочных дисперсий для каждой группы и вычисление поправленных дисперсий
         def varianceGroup1 = new DescriptiveStatistics(group1).getVariance()
         def varianceGroup2 = new DescriptiveStatistics(group2).getVariance()
 
-        def correctedVarianceGroup1 = (group1.size() * varianceGroup1) / (group1.size() - 1)
-        def correctedVarianceGroup2 = (group1.size() * varianceGroup2) / (group1.size() - 1)
+        println varianceGroup1+"====="+varianceGroup2
 
-
-        def variances = [correctedVarianceGroup1, correctedVarianceGroup2]
+        def variances = [varianceGroup1, varianceGroup2]
 
         println "Исправленные дисперсии: $variances"
 
-        // 2. Вычисление статистики для критерия Фишера
+        // Вычисление статистики для критерия Фишера
         def fStat = variances.max() / variances.min()
 
-        // 3. Определение степеней свободы
+        // Определение степеней свободы
         def df1 = group1.size() - 1
         def df2 = group2.size() - 1
 
-        // 4. Определение критической области (двусторонний тест)
+        // Определение критической области (двусторонний тест)
         def fDistribution = new FDistribution(df1, df2)
         def criticalValueTwoSidedLower = fDistribution.inverseCumulativeProbability(alpha / 2)
         def criticalValueTwoSidedUpper = fDistribution.inverseCumulativeProbability(1 - alpha / 2)
@@ -41,7 +46,7 @@ class Fisher {
         println "F-статистика: ${fStat}"
         println "Критическая область (двусторонний тест): (${criticalValueTwoSidedLower}, ${criticalValueTwoSidedUpper})"
 
-        // 5. Определение статистической значимости
+        // Определение статистической значимости
         if (fStat < criticalValueTwoSidedLower || fStat > criticalValueTwoSidedUpper) {
             println "Отвергаем нулевую гипотезу. Существует статистическое различие в дисперсиях двух групп."
         } else {
